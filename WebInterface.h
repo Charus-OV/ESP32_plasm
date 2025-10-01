16ca374837391e02435ce92f53e86f61679c6fdd
Charus-OV <45481064+Charus-OV@users.noreply.github.com>
2025-10-01 16:23:08 +0300

    Update WebInterface.h

--- WebInterface.h
+++ WebInterface.h
@@ -4,29 +4,44 @@
 #include <WebServer.h>
 #include <WebSocketsServer.h>
 #include <ArduinoJson.h>
-#include <SD_MMC.h>
 
 class WebInterface {
 public:
     static void init();
     static void handleClient();
+    static void sendGCodeResponse(const String& response);
+    static void sendGCodeState(const String& state);
     
-    // Статические методы для обработки файлов
-    static void handleFileUpload();
-    static void handleFileList();
-    static void handleFileDelete();
-    static void handleFileRun();
-
 private:
     static WebServer server;
     static WebSocketsServer webSocket;
     
+    // HTTP обработчики
     static void handleRoot();
     static void handleAPIStatus();
     static void handleAPIFiles();
+    static void handleSetZero();
+    static void handleGCodeCommand();
+    
+    // Обработчики файлов
+    static void handleFileUpload();
+    static void handleFileList();
+    static void handleFileDelete();
+    static void handleFileRun();
+    
+    // WebSocket обработчики
     static void handleWebSocket(uint8_t num, WStype_t type, uint8_t* payload, size_t length);
     static void processWebSocketCommand(String message);
     static void sendSystemState();
+    
+    // G-code функции
+    static void startGCodeFromLine(const String& filename, int startLine);
+    static void processNextGCodeLine();
+    static void executeGCodeLine(const String& line);
+    static void handlePlasmaOutage();
+    static void resumeAfterPlasmaRecovery();
+    
+    // HTML страница
     static String getMainPage();
 };
 
